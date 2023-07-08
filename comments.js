4ba9f1c8e42401ace634a90c688b1a389c6666ea
antoniorbr <106894537+antoniorbr@users.noreply.github.com>
2023-07-08 00:14:36 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,43 @@
+// Create web server application with the ability to add comments to the page
+// and view a list of comments.
+// The comments should be stored in a file on the server.
+// The application must meet the following requirements:
+// 1. The application should be a web server.
+// 2. The application should have a main page accessible at http://localhost:8080/.
+// 3. The main page should contain a form with fields:
+//    - Name (text field)
+//    - Message (text area)
+//    - Submit button
+// 4. The application should have a page for viewing comments at http://localhost:8080/comments.
+// 5. The page for viewing comments should contain a list of comments with the following fields:
+//    - Name
+//    - Date and time of adding
+//    - Message
+// 6. The application should have a static folder for static files.
+// 7. The application should have a folder for storing comments.
+// 8. The application should have a file for storing comments.
+// 9. The application should have a file for storing comments.
+// 10. The application should have a module for working with comments.
+// 11. The application should have a module for working with comments.
+// 12. The application should have a module for working with comments.
+// 13. The application should have a module for working with comments.
+// 14. The application should have a module for working with comments.
+// 15. The application should have a module for working with comments.
+// 16. The application should have a module for working with comments.
+// 17. The application should have a module for working with comments.
+// 18. The application should have a module for working with comments.
+// 19. The application should have a module for working with comments.
+// 20. The application should have a module for working with comments.
+// 21. The application should have a module for working with comments.
+// 22. The application should have a module for working with comments.
+// 23. The application should have a module for working with comments.
+// 24. The application should have a module for working with comments.
+// 25. The application should have a module for working with comments.
+// 26. The application should have a module for working with comments.
+// 27. The application should have a module for working with comments.
+// 28. The application should have a module for working with comments.
+
+
+
+
+
